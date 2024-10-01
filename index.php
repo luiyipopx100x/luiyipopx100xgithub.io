c36967f42bf60d51af98cbd5ea4ced7875675582
luiyipopx100x <luistech140616@gmail.com>
2024-09-30 18:59:03 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="estilo.css">
+    <img src="logo-palacio-de-hierro.png" alt="" id="" height="750px" width="1510px"> 
+</head>
+<body>
+<div class="formulario">
+    <h1>INICIO DE SESIÓN</h1>   
+        <form method="post">
+            <div class="username">
+                <input type="text" required>
+                <label>Usuario:</label>
+    </div>
+    <div class="contraseña">
+         <input type="pasword" required>
+            <label>contraseña:</label>
+          
+</div>
+
+<input type="submit" value="Ingresar" class="btn btn-primary">
+<div class="registrarse">
+<a href="registro.php">Registrar usuario</a>
+</div>
+</body>
+</html>
